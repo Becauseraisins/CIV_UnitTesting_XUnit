93e95149769683ba7fde87d42dadf782ea58f0fc
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-11-10 19:34:04 +1100

    i dont remember doing this

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -6,12 +6,36 @@ namespace Tests
 {
     public class UnitTest1
     {
-        [Fact]
+         [Fact]
         public void Test1()
         {
-            RecursiveCalc calc = new RecursiveCalc();
+           RecursiveCalc calc = new RecursiveCalc();
 
-            Assert.Throws<DivideByZeroException>(() => calc.PrimeCheckRec(9, 1));
+           Assert.Throws<DivideByZeroException>(() => calc.PrimeCheckRec(9, 1));
+         }
+
+        
+        [Theory]
+        [InlineData(false, 6)]
+        [InlineData(true, 5)]
+        public void FibbonaciTests(Boolean actual, int num)
+        {
+            RecursiveCalc calc = new RecursiveCalc();
+            Assert.Equal(actual, calc.FibCheck(num));
         }
+         [Theory]
+         [InlineData(false,8)]
+         public void PrimeCheckTest(bool actual, int num){
+              RecursiveCalc calc = new RecursiveCalc();
+             Assert.Equal(actual, calc.PrimeCheckRec(num, 1));
+         }
+
+         [Fact]
+         public void FibbonaciRecursiveTest()
+         {
+             RecursiveCalc calc = new RecursiveCalc();
+             Assert.Equal(1, calc.FibonacciRec(7,7,7));
+         }
+        
     }
 }
